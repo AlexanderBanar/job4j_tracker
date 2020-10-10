8ebe669cf285a1c1b75d90492fce0241c86b9384
AlexanderBanar <s595659@mail.ru>
2020-10-10 09:57:15 +0300

    0. Сортировка [#333461]

---
+++ src/test/java/ru/job4j/tracker/ItemSortByNameTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ItemSortByNameTest {
+    @Test
+    public void whenAscendingSort() {
+        Item first = new Item();
+        first.setId(1);
+        Item second = new Item();
+        second.setId(2);
+        Item third = new Item();
+        third.setId(3);
+        List<Item> items = Arrays.asList(third, first, second);
+        List<Item> expected = Arrays.asList(first, second, third);
+        Collections.sort(items);
+        assertThat(items, is(expected));
+    }
+
+    @Test
+    public void whenDescendingSort() {
+        Item first = new Item();
+        first.setId(1);
+        Item second = new Item();
+        second.setId(2);
+        Item third = new Item();
+        third.setId(3);
+        List<Item> items = Arrays.asList(first, second, third);
+        List<Item> expected = Arrays.asList(third, second, first);
+        Collections.sort(items, Collections.reverseOrder());
+        assertThat(items, is(expected));
+    }
+
+    @Test
+    public void whenSortByName() {
+        Item first = new Item();
+        first.setName("abcde");
+        Item second = new Item();
+        second.setName("cdbea");
+        Item third = new Item();
+        third.setName("edcba");
+        List<Item> items = Arrays.asList(third, second, first);
+        List<Item> expected = Arrays.asList(first, second, third);
+        Collections.sort(items, new ItemSortByName());
+        assertThat(items, is(expected));
+    }
+}
