0cc9a312944c6b0b28963cd4aba3ba6f8c1d1013
AlexanderBanar <s595659@mail.ru>
2020-10-21 20:22:35 +0300

    Отсортировать департаменты [#333453] _ refactor

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,11 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] o1Arr = o1.split("/");
         String[] o2Arr = o2.split("/");
-        int firstElsComp = o2Arr[0].compareTo(o1Arr[0]);
-        if (firstElsComp != 0) {
-            return firstElsComp;
-        } else {
-            return o1.compareTo(o2);
-        }
+        int rsl = o2Arr[0].compareTo(o1Arr[0]);
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 }
