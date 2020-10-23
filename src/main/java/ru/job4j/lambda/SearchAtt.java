0236ea7c27095788c91c09d410ba46beba5a67d1
AlexanderBanar <s595659@mail.ru>
2020-10-23 18:27:51 +0300

    2.0. Встроенные функциональные интерфейсы. [#333304] _ refactored

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -7,40 +7,32 @@ import java.util.function.Predicate;
 public class SearchAtt {
     public static List<Attachment> filterSize(List<Attachment> list) {
         List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : list) {
-            if (att.getSize() > 100) {
-                rsl.add(att);
-            }
-        }
-        return rsl;
-    }
-
-    public static List<Attachment> filterName(List<Attachment> list) {
-        List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : list) {
-            if (att.getName().contains("bug")) {
-                rsl.add(att);
-            }
-        }
-        return rsl;
-    }
-
-    public static List<Attachment> filter(List<Attachment> list) {
-        Predicate<Attachment> test1 = new Predicate<Attachment>() {
+        Predicate<Attachment> tester = new Predicate<Attachment>() {
             @Override
             public boolean test(Attachment attachment) {
                 return attachment.getSize() > 100;
             }
         };
-        Predicate<Attachment> test2 = new Predicate<Attachment>() {
+        rsl = filter(rsl, tester);
+        return rsl;
+    }
+
+    public static List<Attachment> filterName(List<Attachment> list) {
+        List<Attachment> rsl = new ArrayList<>();
+        Predicate<Attachment> tester = new Predicate<Attachment>() {
             @Override
             public boolean test(Attachment attachment) {
                 return attachment.getName().contains("bug");
             }
         };
+        rsl = filter(rsl, tester);
+        return rsl;
+    }
+
+    public static List<Attachment> filter(List<Attachment> list, Predicate<Attachment> tester) {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : list) {
-            if (test1.test(att) || test2.test(att)) {
+            if (tester.test(att)) {
                 rsl.add(att);
             }
         }
