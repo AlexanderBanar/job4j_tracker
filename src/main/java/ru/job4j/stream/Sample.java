cc8a951ca010ed36cae78d7b5f06388d00783b20
AlexanderBanar <s595659@mail.ru>
2020-10-26 21:01:51 +0300

    0. Stream API [#333310]

---
+++ src/main/java/ru/job4j/stream/Sample.java
@@ -0,0 +1,22 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Sample {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(-5);
+        list.add(0);
+        list.add(57);
+        list.add(-79);
+        list.add(4000);
+        List<Integer> rsl = list.stream().filter(
+                (x) -> x >= 0
+        ).collect(Collectors.toList());
+        System.out.println(rsl);
+    }
+}
